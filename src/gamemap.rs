10cac31a629763be9dd9e4e1ebe2afe4d504693a
jcerise <jcerise06@gmail.com>
2023-09-18 23:40:49 -0600

    Added very basic map generation, selects a random tile, and draws a map to the screen with a repeating series of the chosen tile. Next up is actual map generation.

---
+++ src/gamemap.rs
@@ -0,0 +1,70 @@
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng
+};
+use crate::prelude::*;
+
+const WORLD_INDEX_SIZE: usize = 55;
+const NUM_TILES: usize = (MAP_WIDTH * MAP_HEIGHT) as usize;
+
+fn coords_to_index(x: usize, y: usize) -> usize {
+    ((y * WORLD_INDEX_SIZE) + x)
+}
+
+pub enum TileType {
+    Grass1,
+    Grass2,
+    Grass3,
+    Grass4,
+}
+
+impl Distribution<TileType> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> TileType {
+        match rng.gen_range(0..=2) { // rand 0.8
+            0 => TileType::Grass1,
+            1 => TileType::Grass2,
+            2 => TileType::Grass3,
+            _ => TileType::Grass4,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+pub struct Tile {
+    pub index: usize,
+}
+
+impl Tile {
+    fn from_tile_type(tile_type: TileType) -> Tile {
+        match tile_type {
+            TileType::Grass1 => Tile { index: coords_to_index(28, 33) },
+            TileType::Grass2 => Tile { index: coords_to_index(29, 33) },
+            TileType::Grass3 => Tile { index: coords_to_index(30, 33) },
+            TileType::Grass4 => Tile { index: coords_to_index(31, 33) }
+        }
+    }
+}
+
+pub struct GameMap {
+    pub tiles: Vec<Tile>
+}
+
+impl GameMap {
+    pub fn new() -> Self {
+        let grass_tile: TileType = rand::random();
+        Self{
+            tiles: vec![
+                Tile::from_tile_type(grass_tile); NUM_TILES]
+        }
+    }
+
+    fn map_index(x: usize, y: usize) -> usize {
+        ((y * MAP_HEIGHT) + x) as usize
+    }
+
+     pub fn index_to_map(index: usize) -> (usize, usize) {
+        let x = index % MAP_WIDTH;
+        let y = index / MAP_WIDTH;
+        (x, y)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod map;
+mod gamemap;
 use bevy::asset::LoadState;
 
 // Sprites are 24x24
@@ -6,11 +6,25 @@ use bevy::asset::LoadState;
 
 mod prelude {
     pub use bevy::prelude::*;
-    pub const SCREEN_WIDTH: f32 = 1024.0;
-    pub const SCREEN_HEIGHT: f32 = 768.0;
+    pub const SCREEN_WIDTH: usize = 1024;
+    pub const SCREEN_HEIGHT: usize = 768;
+    pub const MAP_WIDTH: usize = 40;
+    pub const MAP_HEIGHT: usize = 30;
+    pub const TILE_SIZE: f32 = 24.0;
+    pub use crate::gamemap::*;
 }
 
 use prelude::*;
+
+const HALF_TILE_SIZE: f32 = TILE_SIZE / 2.0;
+const MAP_TILE_WIDTH: usize = MAP_WIDTH;
+const MAP_TILE_HEIGHT: usize = MAP_HEIGHT;
+const HALF_MAP_PIXEL_WIDTH: f32 =
+    MAP_TILE_WIDTH as f32 * TILE_SIZE as f32 / 2.0;
+const HALF_MAP_PIXEL_HEIGHT: f32 =
+    MAP_TILE_HEIGHT as f32 * TILE_SIZE / 2.0;
+const WIDTH_CENTER_OFFSET: f32 = HALF_MAP_PIXEL_WIDTH - HALF_TILE_SIZE;
+const HEIGHT_CENTER_OFFSET: f32 = HALF_MAP_PIXEL_HEIGHT - HALF_TILE_SIZE;
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
 enum AppState {
@@ -31,7 +45,7 @@ fn main() {
             WindowPlugin {
                 primary_window: Some(Window {
                     title: "Northlands".into(),
-                    resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
+                    resolution: (SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32).into(),
                     ..default()
                 }),
                 ..default()
@@ -93,39 +107,39 @@ fn animate_sprite_two_frame(
     }
 }
 
-fn coords_to_sprite(x: u16, y: u16) -> usize {
-    ((y * 18) + x) as usize
-}
-
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let terrain_handle = asset_server.get_handle("textures/creatures.png");
-    let texture_atlas = TextureAtlas::from_grid(terrain_handle, Vec2::new(24.0, 24.0), 18, 22, None, None);
+    let terrain_handle = asset_server.get_handle("textures/world.png");
+    let texture_atlas = TextureAtlas::from_grid(terrain_handle, Vec2::new(24.0, 24.0), 55, 39, None, None);
     let atlas_handle = texture_atlases.add(texture_atlas);
-
-    let index_start: usize = coords_to_sprite(14, 4);
-    let index_end: usize = coords_to_sprite(14, 5);
-
-    let animation_indices = AnimationIndices { first: index_start, last: index_end  };
 
     // set up a scene to display our texture atlas
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        SpriteSheetBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
-                scale: Vec3::splat(1.0),
+    // Init a map
+    let map: GameMap = GameMap::new();
+
+    // Render the tiles that compose the map to the screen
+    for (index, tile) in map.tiles.iter().enumerate() {
+        let coords = GameMap::index_to_map(index);
+        commands.spawn((
+            SpriteSheetBundle {
+                transform: Transform {
+                    translation: Vec3::new(
+                        coords.0 as f32 * TILE_SIZE - WIDTH_CENTER_OFFSET,
+                        coords.1 as f32 * TILE_SIZE - HEIGHT_CENTER_OFFSET,
+                        0.0
+                    ),
+                    scale: Vec3::splat(1.0),
+                    ..default()
+                },
+                sprite: TextureAtlasSprite::new(tile.index),
+                texture_atlas: atlas_handle.clone(),
                 ..default()
-            },
-            sprite: TextureAtlasSprite::new(animation_indices.first),
-            texture_atlas: atlas_handle.clone(),
-            ..default()
-        },
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(0.5, TimerMode::Repeating)),
-    ));
+            }
+        ));
+    }
 }
